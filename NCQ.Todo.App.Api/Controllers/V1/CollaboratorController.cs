98d0b5db1ae214afa354df58e4d9335e17c8884a
Luis Orozco <luis.orozco@nativo.la>
2024-04-15 10:33:25 -0600

    Integracion swagger con versionamiento

--- NCQ.Todo.App.Api/Controllers/V1/CollaboratorController.cs
+++ NCQ.Todo.App.Api/Controllers/V1/CollaboratorController.cs
@@ -1,4 +1,9 @@
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using Amazon.Extensions.CognitoAuthentication;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NCQ.Todo.App.Api.Models;
 using NCQ.Todo.App.Application.Features.CollaboratorFeature.GetAllCollaborators;
@@ -7,26 +12,55 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace NCQ.Todo.App.Api.Controllers.V1
 {
     [ApiVersion("1.0")]
+    [Authorize(Policy = "NCQ1")]
     public class CollaboratorController : BaseController
     {
         private readonly IMediator _mediator;
+        private readonly IAmazonCognitoIdentityProvider _cognitoService;
+        private readonly CognitoUserPool _cognitoUserPool;
 
-        public CollaboratorController(IMediator mediator)
+        /*private readonly AmazonCognitoIdentityProviderClient adminAmazonCognitoIdentityProviderClient;
+        private readonly AmazonCognitoIdentityProviderClient anonymousAmazonCognitoIdentityProviderClient;*/
+        //private readonly AWSCredentials awsCredentials;
+
+
+
+        public CollaboratorController(IMediator mediator, IAmazonCognitoIdentityProvider cognitoService, CognitoUserPool cognitoUserPool)
         {
             _mediator = mediator;
+            _cognitoService = cognitoService;
+            _cognitoUserPool = cognitoUserPool;
         }
 
         /// <summary>
         /// Get all collaborators
         /// </summary>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <returns></returns>        
         [HttpGet]
         [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(OkResultData<List<GetAllCollaboratorsResponse>>))]
-        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+        public async Task<IActionResult> GetAllLincecia(CancellationToken cancellationToken)
         {
-            var response = await _mediator.Send(new GetAllCollaboratorsRequest(), cancellationToken);
-            return Ok(response);
+            var x = User;
+            var y = _cognitoUserPool.GetUser("c16be550-7021-701a-8eac-ef254f8a327d");
+
+            var request = new ListUsersRequest
+            {
+                UserPoolId = "us-east-2_9vCkq5idW"
+            };
+
+            var users = new List<UserType>();
+
+            var usersPaginator = _cognitoService.Paginators.ListUsers(request);
+            await foreach (var response in usersPaginator.Responses)
+            {
+                users.AddRange(response.Users);
+            }
+
+            return Ok(users);
+            //var response = "test";
+            //var response = await _mediator.Send(new GetAllCollaboratorsRequest(), cancellationToken);
+            //return Ok(response);
         }
     }
 }
--- NCQ.Todo.App.Api/Program.cs
+++ NCQ.Todo.App.Api/Program.cs
@@ -1,12 +1,20 @@
+using Amazon;
+using Amazon.CognitoIdentityProvider;
+using Amazon.Extensions.CognitoAuthentication;
+using Amazon.Runtime;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NCQ.Todo.App.Api.Common;
 using NCQ.Todo.App.Api.Extensions;
 using NCQ.Todo.App.Application;
 using NCQ.Todo.App.Persistence;
 using NCQ.Todo.App.Persistence.Context;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,6 +46,60 @@ builder.Services.ConfigureApplication();
 builder.Services.ConfigureApiBehavior();
 builder.Services.ConfigureCorsPolicy();
 
+var amazonCognitoIdentityProvider = new AmazonCognitoIdentityProviderClient(RegionEndpoint.USEast2);
+var cognitoUserPool = new CognitoUserPool("us-east-2_9vCkq5idW", "rn7vu5qesihbjb1pfcl9egnr4", amazonCognitoIdentityProvider);
+
+builder.Services.AddSingleton<IAmazonCognitoIdentityProvider>(x => amazonCognitoIdentityProvider);
+builder.Services.AddSingleton(x => cognitoUserPool);
+
+//builder.Services.AddCognitoIdentity();
+
+/*List<string> authorizedDomains = new List<string>()
+    {
+        "NCQ",        
+    };
+
+builder.Services
+    .AddAuthorization(options =>
+    {
+        options.AddPolicy("NCQ", policy => policy.RequireClaim("cognito:groups", authorizedDomains));
+    });*/
+
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKeyResolver = (s, securityToken, identifier, parameters) =>
+            {
+                var data1 = new HttpClient().GetAsync(parameters.ValidIssuer + "/.well-known/jwks.json").Result;
+                var jsonString = data1.Content.ReadAsStringAsync().Result;
+                var value = JsonConvert.DeserializeObject<JsonWebKeySet>(jsonString);
+                return value != null ? (IEnumerable<SecurityKey>)value.Keys : null;
+            },
+
+            ValidIssuer = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_9vCkq5idW",
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateAudience = true,
+            AudienceValidator = (IEnumerable<string> audiences, SecurityToken securityToken, TokenValidationParameters validationParameters) =>
+            {
+                var castedToken = securityToken as JwtSecurityToken;
+                var clientId = castedToken?.Claims.FirstOrDefault(x => x.Type == "client_id")?.Value;
+                // Do Nothing
+                return clientId == "6m7hj7g2vpcjh3rll3fc6or55o";
+            },
+
+        };
+        options.MetadataAddress = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_9vCkq5idW/.well-known/openid-configuration";
+    });
+
 
 //  Set API Version
 builder.Services.AddApiVersioning(opt =>
@@ -59,9 +121,11 @@ builder.Services.AddVersionedApiExplorer(setup =>
 
 var app = builder.Build();
 
+app.UseAuthentication();
+
 var serviceScope = app.Services.CreateScope();
 var dataContext = serviceScope.ServiceProvider.GetService<DataContext>();
-dataContext?.Database.EnsureCreated();
+//dataContext?.Database.EnsureCreated();
 
 
 var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
@@ -80,7 +144,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseErrorHandler();
+//app.UseErrorHandler();
 app.UseCors();
 
 app.UseHttpsRedirection();
@@ -90,3 +154,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
